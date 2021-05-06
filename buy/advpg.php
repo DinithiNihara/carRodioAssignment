de07ffcb8a7fee7e037bf2fab4b437f8d91a6130
DinithiNihara <80022575+DinithiNihara@users.noreply.github.com>
2021-05-06 22:41:56 +0530

    comment

--- buy/advpg.php
+++ buy/advpg.php
@@ -10,6 +10,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+if (isset($_POST['submit'])) { // Check press or not Post Comment Button
+	$name = $_POST['name']; // Get Name from form
+	$email =$_SESSION['email']; // Get Email from form
+	$comment = $_POST['comment']; // Get Comment from form
+
+	$sql = "INSERT INTO comments (name, email, comment)
+			VALUES ('$name', '$email', '$comment')";
+	$result = mysqli_query($conn, $sql);
+	if ($result) {
+		echo "<script>alert('Comment added successfully.')</script>";
+	} else {
+		echo "<script>alert('Comment does not add.')</script>";
+	}
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +39,7 @@ if ($conn->connect_error) {
     <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/homepg.css" rel="stylesheet">
     <link href="../css/sellpg.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/comment.css">
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -402,6 +420,48 @@ if ($conn->connect_error) {
             </table>
         </form>
     </div>
+    <div class="wrapper">
+                <form action="" method="POST" class="form">
+                    <div class="row">
+                        <div class="input-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" placeholder="Enter your Name" required>
+                        </div>
+                        <div class="input-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email" value=<?php echo $_SESSION['email']?> required>
+                        </div>
+                    </div> 
+                    <div class="input-group textarea">
+                        <label for="comment">Comment</label>
+                        <textarea id="comment" name="comment" placeholder="Enter your Comment" required></textarea>
+                    </div>
+                    <div class="input-group">
+                        <button name="submit" class="btn">Post Comment</button>
+                    </div>
+                </form>
+                <div class="prev-comments">
+                    <?php 
+                    
+                    $sql = "SELECT * FROM comments";
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+
+                    ?>
+                    <div class="single-item">
+                        <h4><?php echo $row['name']; ?></h4>
+                        <a href="mailto:<?php echo $row['email']; ?>"><?php echo $row['email']; ?></a>
+                        <p><?php echo $row['comment']; ?></p>
+                    </div>
+                    <?php
+
+                        }
+                    }
+                    
+                    ?>
+                </div>
+	        </div> 
 
     <script>
         document.getElementsByClassName("ss1")[0].classList.add("active");
